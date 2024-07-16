9b0238346febefcc5f3fbcf212126c097c8a242a
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-16 13:55:44 +0700

    refactor: use my own solution

--- mumbling.js
+++ mumbling.js
@@ -1,15 +1,14 @@
 // url: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/fork/javascript
 function accum(s) {
-  function capitalize(s) {
-    return s.toLowerCase().replace(/\b./g, function (a) {
-      return a.toUpperCase();
-    });
+  let resArr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let index = i;
+    while (index >= 0) {
+      resArr.push(index === i ? s[i].toUpperCase() : s[i].toLowerCase());
+      index--;
+    }
+    i < s.length - 1 && resArr.push("-");
   }
-  var a = s.split(""),
-    res = "";
-  for (var i = 0; i < a.length; i++) {
-    for (var j = 0; j < i + 1; j++) res += a[i];
-    res += "-";
-  }
-  return capitalize(res.substr(0, res.length - 1));
+  return resArr.join("");
 }
