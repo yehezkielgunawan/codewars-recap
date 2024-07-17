20cd993a522da746bfa47d4b064f590aa005a467
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-17 14:07:05 +0700

    reverse triangle challenge

---
+++ reversetriangle1.js
@@ -0,0 +1,12 @@
+function createReverseTriangle(n) {
+  let triangleRes = "";
+  for (let i = n; i > 0; i--) {
+    for (j = 0; j < i; j++) {
+      triangleRes += "#";
+    }
+    triangleRes += "\n";
+  }
+  return triangleRes;
+}
+
+console.log(createReverseTriangle(8));
