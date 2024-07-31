239e4d05eaf7cf682924508ba812e0003cf5519f
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-31 17:51:57 +0700

    feat: Messi Goals challenge

---
+++ Grasshopper - Messi goals function.js
@@ -0,0 +1,8 @@
+// codewars link: https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+console.log(goals(0, 0, 0));
+console.log(goals(5, 10, 2));
