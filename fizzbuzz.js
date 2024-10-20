ab8c2537dbe6df8450b7a21d9c516c27c4940e61
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-10-20 18:54:48 +0700

    feat: crocoachSpeed.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,16 @@
-function fizzBuzz(nums) {
-  for (let num of nums) {
-    if (num % 3 === 0 && num % 5 === 0) {
+// fizzbuzz is a function that takes an array of numbers and logs "Fizz" if the number is divisible by 3, "Buzz" if the number is divisible by 5, "FizzBuzz" if the number is divisible by both 3 and 5, and the number if it is not divisible by 3 or 5.
+function fizzBuzz(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
       console.log("FizzBuzz");
-    } else if (num % 3 === 0) {
+    } else if (array[i] % 3 === 0) {
       console.log("Fizz");
-    } else if (num % 5 === 0) {
+    } else if (array[i] % 5 === 0) {
       console.log("Buzz");
     } else {
-      console.log(num);
+      console.log(array[i]);
     }
   }
 }
 
-fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15]);
