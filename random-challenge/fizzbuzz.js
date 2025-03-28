94f6d85c5841dc31ca677aa725afff44605b0085
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-03-28 12:28:17 +0700

    feat: modify fizzbuzz function to return an array instead of logging output

--- random-challenge/fizzbuzz.js
+++ random-challenge/fizzbuzz.js
@@ -1,16 +1,17 @@
-// fizzbuzz is a function that takes an array of numbers and logs "Fizz" if the number is divisible by 3, "Buzz" if the number is divisible by 5, "FizzBuzz" if the number is divisible by both 3 and 5, and the number if it is not divisible by 3 or 5.
-function fizzBuzz(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (array[i] % 3 === 0) {
-      console.log("Fizz");
-    } else if (array[i] % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(array[i]);
-    }
-  }
-}
+// Return an array
+function fizzbuzz(n) {
+	const resArr = [];
 
-fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15]);
+	for (let i = 1; i <= n; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			resArr.push("FizzBuzz");
+		} else if (i % 3 === 0) {
+			resArr.push("Fizz");
+		} else if (i % 5 === 0) {
+			resArr.push("Buzz");
+		} else {
+			resArr.push(i);
+		}
+	}
+	return resArr;
+}
