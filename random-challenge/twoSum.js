a5df4af59275810282d5943ebad9f9bbcced0b6d
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-04-20 18:20:27 +0700

    test: add test cases for twoSum function

--- random-challenge/twoSum.js
+++ random-challenge/twoSum.js
@@ -10,3 +10,7 @@ function twoSum(numbers, target) {
 		tempNums.set(numbers[i], i);
 	}
 }
+
+// test cases
+console.log(twoSum([1, 2, 3], 4)); // [0, 2]
+console.log(twoSum([1234, 5678, 9012], 14690)); // [1, 2]
